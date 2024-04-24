315b4f31912e425ef65fdbc9d45b8398b054ad82
tariqkalsaidi <it.tareq.alsaidi@gmail.com>
2024-04-25 01:47:29 +0300

    init

---
+++ aljood_admin/config/DBSettings.php
@@ -0,0 +1,22 @@
+<?php
+//INSTALL FLYWEB
+class DatabaseSettings
+{
+	var $settings;
+	function getSettings()
+	{
+		// install
+		// Database variables
+		// Host name
+		$settings['dbhost'] = 'localhost';//'localhost';
+		// Database name
+		$settings['dbname'] = 'aylaxjlf_aljoodapp';//'aylaxjlf_aljoodapp';
+		// Username
+		$settings['dbusername'] = 'aylaxjlf_aljoodapp';//'aylaxjlf_aljoodapp';
+		// Password 
+		$settings['dbpassword'] = '#aljoodapp@2024';//'#aljoodapp@2024';
+
+		return $settings;
+	}
+} 
+
---
+++ aljood_app/android/app/src/main/kotlin/com/aljoodlaw/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aljoodlaw
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
